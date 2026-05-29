4bf2c79ed96aab2320f9df4bd6487287d1789830
n.polyko <n.polyko@jetmail.cc>
2026-05-30 01:55:12 +0300

    Update locator class name in checkout test for accuracy

--- framework/tests/checkout.spec.ts
+++ framework/tests/checkout.spec.ts
@@ -24,7 +24,7 @@ test("successful checkout flow", async ({ page }) => {
 
   await page.locator('[data-test="finish"]').click();
 
-  await expect(page.locator(".complete-header-test2")).toContainText(
-    "Thank you for your order!",
-  );
+  await expect(
+    page.locator(".complete-header-test21231231231231231231231321"),
+  ).toContainText("Thank you for your order!");
 });
